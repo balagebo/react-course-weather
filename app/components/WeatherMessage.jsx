d82bada07d8e410c94f07cf4ce278d7ad8490084
Bóta Balázs (PW) <bota.balazs@pwstudio.hu>
2017-07-06 21:33:35 +0200

    Style weather form

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -2,9 +2,9 @@ const React = require('react');
 
 var WeatherMessage = ({temp, location}) => {
     return (
-        <div>
+        <h3 className="text-center">
             It's {temp} in {location}
-        </div>
+        </h3>
     );
 };
 
